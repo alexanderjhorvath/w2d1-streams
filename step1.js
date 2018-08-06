d4e4615fac510e9767917492e6368d99968a3c72
Alexander Horvath <tantousha@me.com>
2018-08-06 18:58:18 +0000

    completed step 1

--- step1.js
+++ step1.js
@@ -1,21 +1,23 @@
 var https = require('https');
 
 function getAndPrintHTMLChunks () {
-
+  var data = '';
 
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
 
-  // borrowed from template provided in lesson.
+  // borrowed from template provided in lesson and working with Kyle.
   https.get(requestOptions, function (response) {
     response.setEncoding('utf8');
     response.on('data', function (data) {
-      console.log('Chunk Received. Length: ', data.length);
+      console.log(data, '\nChunk Received. Length: ', data.length);
     });
+
     response.on('end', function() {
       console.log('Response stream complete.');
+      console.log(data);
     });
   });
 }
