55b31021db6bf8bea3fa999cc3edd35b41a650f2
Alexander Horvath <tantousha@me.com>
2018-08-06 18:46:28 +0000

    created step1 file

---
+++ step1.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  // borrowed from template provided in lesson.
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length: ', data.length);
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
